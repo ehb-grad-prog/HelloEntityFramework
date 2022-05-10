bda4fdd7745797e4842b7a1bfaedc48e98d58799
Wannes Gennar <wannes.gennar@ehb.be>
2022-05-10 18:17:16 +0200

    Initial commit

---
+++ HelloEntityFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloEntityFramework.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
